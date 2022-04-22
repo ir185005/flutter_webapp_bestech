43e62145303009fc8e5a519e62747149115390cb
Ildar Rakhmatullin <ir185005@mail.ru>
2022-04-22 14:41:54 +0300

    Initial commit

---
+++ lib/widgets/main_heading.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_webapp_bestech/widgets/responsive.dart';
+import 'package:flutter/material.dart';
+
+class MainHeading extends StatelessWidget {
+  const MainHeading({
+    Key? key,
+    required this.screenSize,
+  }) : super(key: key);
+
+  final Size screenSize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(
+        top: screenSize.height / 10,
+        bottom: screenSize.height / 15,
+      ),
+      width: screenSize.width,
+      child: Text(
+        'Knowledge diversity',
+        textAlign: TextAlign.center,
+        style: TextStyle(
+          fontSize: 40,
+          color: Color(0xff077bd7),
+          fontFamily: 'Montserrat',
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
